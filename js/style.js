9b6731a1dca7534888ff662514282a879a2aeb7c
hm <meihuang27@foxmail.com>
2018-10-13 19:29:00 +0800

    首页和登录页面样式完成

--- js/style.js
+++ js/style.js
@@ -73,6 +73,7 @@ $.extend({
 				this.indexA = 0;
 			}
 			this.slider();
+			
 		}.bind(this));
 		//小圆点移入事件
 		var that = this;
@@ -98,21 +99,77 @@ $.extend({
 		$('.ollis').eq(this.indexA).css("background","#cf0006");
 	},
 	autoPlay : function(){
+		clearInterval(this.timer);
 		var that = this;
 		this.timer = setInterval(()=>{
 			this.indexA ++;
+			console.log(this.indexA)
 			if(this.indexA == this.num){
 				this.indexA = 0;
 			}
 			this.slider();
 		},2000);
-		console.log(this.oBigBox)
+		//console.log(this.oBigBox)
 		this.oBigBox.mouseenter(function(){
-			clearInterval(that.timer);
-		})
+			clearInterval(this.timer);
+		}.bind(this))
 		this.oBigBox.mouseleave(function(){
-			that.autoPlay();
-		})
+			this.autoPlay();
+		}.bind(this))
+//		this.oBigBox.hover(function(){
+//			//alert()
+//			clearInterval(that.timer);
+//		},function(){
+//			this.autoPlay()
+//		}.bind(this))
 	}
 })
 $.fnInit('#banner');
+
+
+//回到顶部部分
+$('.hui .hui-last').click(function(){
+	$('html,body').animate({"scrollTop":0},2000);
+})
+//优先推荐部分使用ajax请求
+$.ajax({
+		type:"get",
+		url:"tpic.json",
+		async:true,
+		success : function(arr){
+			//console.log(arr[0]);
+			var str= `
+				<p>
+					<a href="#">
+						<img src="${arr[0]}" alt="" />
+					</a>
+				</p>
+			`;
+			$('.y-t1').prepend(str);
+		}
+	})
+//下装推荐ajax请求
+$.ajax({
+	type:"get",
+		url:"zpic.json",
+		async:true,
+		success : function(arr){
+				var brr = [];
+				for(var i = 0,len = arr.length;i < len;i ++){
+					//console.log(arr);
+					var str= `
+						<p>
+							<a href="#">
+								<img src="${arr[i]}" alt="" />
+							</a>
+						</p>
+					`;
+					brr.push(str);					
+				}
+				$('.x-z2').prepend(brr[0]);
+				$('.x-z3').prepend(brr[1]);
+				$('.x-z4').prepend(brr[2]);
+				$('.x-z1').prepend(brr[3]);
+				//console.log(brr[0]);				
+		}
+})
